9313afc9278df1f45ffa346e1a3f8b6785e72c32
Kacper Goławski <kacpergolawski@gmail.com>
2017-10-28 16:09:37 +0200

    xx

--- Car factory/Program.cs
+++ Car factory/Program.cs
@@ -17,6 +17,7 @@ namespace Car_factory
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
