4dfad17e59dfbd0cd9ddbe5b7eecfac3e2bc58bd
Kacper Goławski <kacpergolawski@gmail.com>
2017-10-28 16:06:55 +0200

    testetsette

--- Car factory/Program.cs
+++ Car factory/Program.cs
@@ -14,6 +14,7 @@ namespace Car_factory
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
